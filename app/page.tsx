3c5a15eefc8bb15f3e02adefb6e5bc62ccefe170
pqptri <admin@phamtri.com>
2026-03-25 23:21:17 +0700

    update homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+console.log("PAGE TSX MOI DA CHAY");
 export default function Home() {
   return (
     <main className="min-h-screen flex items-center justify-center bg-red-500 text-white text-5xl font-bold">
